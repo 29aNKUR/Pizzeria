41610b127305557d516f1881a4fd4513e9325bbb
Ankur Semle <semleankur@gmail.com>
2024-02-26 22:50:54 +0530

    add Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Header from './components/Header'
 import OrderForm from './components/OrderForm'
+import Tracker from './components/Tracker'
 
 function App() {
 
   return (
-    <div className='bg-slate-300'>
+    <div>
     <Header />
-    <OrderForm />
+    <div className='bg-slate-300'>    <OrderForm /></div>
+
+    <Tracker />
     </div>
   )
 }
--- src/components/Tracker.tsx
+++ src/components/Tracker.tsx
@@ -1,7 +1,15 @@
 const Tracker = () => {
   return (
-    <div>Tracker</div>
-  )
-}
+    <div>
+      <h1 className="flex justify-center mt-10 font-bold text-3xl">Tracker</h1>
+      <div className="flex justify-around mt-10 font-bold text-xl">
+        <div>Order Placed</div>
+        <div>Order in Making</div>
+        <div>Order Ready</div>
+        <div>Order Picked</div>
+      </div>
+    </div>
+  );
+};
 
-export default Tracker
+export default Tracker;
